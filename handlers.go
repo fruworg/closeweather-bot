45746fea0896e148f1df844037cd939d8573ba50
Mitch Conner <96399261+fruworg@users.noreply.github.com>
2022-01-05 13:42:17 +0500

    Update handlers.go

--- handlers.go
+++ handlers.go
@@ -26,7 +26,7 @@ type OWM struct {
 }
 
 func (a *application) startHandler(m *tbot.Message) {
-	msg := "\n*Привет!* Чтобы начать, отправь город в чат.\n\nКоманда */today* - прогноз на сегодня.\nКоманда */week* - на 5 дней."
+	msg := "\n*Привет!* Чтобы начать, отправь город в чат.\nКоманда */today* - прогноз на сегодня.\nКоманда */week* - на 5 дней."
 	a.client.SendMessage(m.Chat.ID, msg, tbot.OptParseModeMarkdown)
 
 // Handle the msg command here
