1eda368afdf34255299531fea3052de099766135
Mitch Conner <96399261+fruworg@users.noreply.github.com>
2022-01-08 12:22:01 +0500

    Update handlers.go

--- handlers.go
+++ handlers.go
@@ -176,7 +176,7 @@ func (a *application) msgHandler(m *tbot.Message) {
 						}
 						if m.Text == "/week" && date != cdate {
 							if ((st[len(st)-3] == "06:00:00" || st[len(st)-3] == "18:00:00") && datecheck < 4) ||
-								(st[len(st)-3] == "18:00:00" && datecheck == 4) {
+								(st[len(st)-3] == "12:00:00" && datecheck == 4) {
 								if st[len(st)-3] == "06:00:00" || datecheck == 0{
 									datecheck++
 									msg = msg + "\n\n> Прогноз на " + date
