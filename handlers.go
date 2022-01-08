6a4d622855118e91360e6376f042753006218aad
Mitch Conner <96399261+fruworg@users.noreply.github.com>
2022-01-08 07:10:07 +0500

    Update handlers.go

--- handlers.go
+++ handlers.go
@@ -168,9 +168,9 @@ func (a *application) msgHandler(m *tbot.Message) {
 			dt := strings.Split(time.Now().In(loc).Format("2006-01-02"), "-")
 			urldate := fmt.Sprintf("%s%s%s", dt[0], dt[1], dt[2])
 			if citycodes[strings.ToLower(city)] != ""{
-				urldate = citycodes[strings.ToLower(city)]
+				urldate = citycodes[strings.ToLower(city)] + "_" + urldate
 			}
-			url = "https://tesis.lebedev.ru/upload_test/files/kp_" + urldate + ".png?bg=2"
+			url = "https://tesis.lebedev.ru/upload_test/files/kp_" + urldate + ".png?bg=1"
 		}
 	case "/week":
 		city, err := client.Get(m.Chat.ID).Result()
@@ -209,10 +209,7 @@ func (a *application) msgHandler(m *tbot.Message) {
 					msg = fmt.Sprintf("%v", len(val.List))
 				}
 			}
-			loc, _ := time.LoadLocation("Europe/Moscow")
-			dt := strings.Split(time.Now().In(loc).Format("2006-01-02"), "-")
-			urldate := fmt.Sprintf("%s%s%s", dt[0], dt[1], dt[2])
-			url = "https://tesis.lebedev.ru/upload_test/files/fc_" + urldate + ".png"
+			url = "https://tesis.lebedev.ru/upload_test/files/fc_" + dt + ".png"
 		}
 	default:
 		m.Text = strings.TrimRight(m.Text, " .!")
